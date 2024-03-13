9fb1235da88b9604c9919cd9bc3b762b4b5285c1
sarthak mohanty ☻ <sarthaktexas@gmail.com>
2024-03-12 20:19:18 -0500

    Remove shortlinks from nav

--- components/nav.js
+++ components/nav.js
@@ -7,17 +7,16 @@ export default function Nav() {
         { href: '/', label: 'home' },
         // { href: '/about', label: 'about' },
         // { href: '/manifesto', label: 'manifesto' },
-        // { href: 'https://www.craft.do/s/5PLFAFavEh6V37', label: 'toolbox' },
+        { href: 'https://www.craft.do/s/5PLFAFavEh6V37', label: 'toolbox' },
         // { href: 'https://www.notion.so/sarthakmohanty/Annual-Review-2020-d8e25a934aa2441fae8cc79a9a1bdbc8', label: 'annual review' },
         // { href: 'https://www.notion.so/sarthakmohanty/Reading-List-2020-10daef3ae49e4b09a1bf6223eaa1b80b', label: 'reading list' },
         // { href: 'https://medium.com/@sarthaktexas', label: 'writing' },
         { href: 'mailto:sarthak@sarthakmohanty.me', label: 'email'},
-        { href: 'https://srtk.me/gh', label: 'github'},
-        { href: 'https://srtk.me/tw', label: 'twitter'},
+        { href: 'https://github.com/sarthaktexas', label: 'github'},
+        { href: 'https://twitter.com/sarthaktexas', label: 'twitter'},
         { href: 'https://read.cv/srtk', label: 'cv'},
         // { href: '/ol/rob-a-bank', label: 'open letter to rob a bank'},
         // { href: '/ol/art', label: 'open letter about art'},
-        // { href: '/manifesto', label: 'manifesto'},
     ]
 
     return (
